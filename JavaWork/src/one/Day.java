f5fbefd90a7d6a84280505533544f6ea67ebca05
SONY <SONY@Reshma-T2544>
2023-07-06 21:22:35 +0530

    DayPrgm

---
+++ JavaWork/src/one/Day.java
@@ -0,0 +1,28 @@
+package one;
+
+public class Day {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sunday");
+
+
+		System.out.println("Monday");
+
+
+		System.out.println("Tuesday");
+
+
+		System.out.println("Wednesday");
+
+
+		System.out.println("Thursday");
+
+
+		System.out.println("Friday");
+
+
+		System.out.println("Saturday");
+	}
+
+}
